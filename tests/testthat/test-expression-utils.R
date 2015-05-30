1b795a790e2f3a048efeb7d3adf8da388e03658c
lianogls <lianogls@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2015-05-30 09:13:51 +0000

    Adds expression-utilties.R for `CPM/RPKM` and `meltx`
    Also has methods to facilitate "universal access" to pData, fData, and
    expression data from the different often-used expression containers, eg.
    ExpressionSet, EList, DGEList, SummarizedExperiment, etc.
    
    git-svn-id: http://resscm.gene.com/bioinfo/projects/R/trunk/multiGSEA@166660 ea8427e4-251f-0410-985a-fb3ea31d2ee2

---
+++ tests/testthat/test-expression-utils.R
@@ -0,0 +1,29 @@
+context("Expression Utilities")
+
+test_that("as.DGEList conversion works", {
+  es <- exampleExpressionSet(do.voom=FALSE)
+
+  y.expect <- calcNormFactors(DGEList(exprs(es), genes=fData(es)))
+  y.expect$samples <- cbind(
+    y.expect$samples,
+    pData(es)[, setdiff(names(pData(es)), names(y.expect$samples))])
+
+  ## From ExpressionSet to DGEList
+  y.es <- as.DGEList(es, 'exprs')
+  expect_equal(y.es$counts, y.expect$counts)
+  expect_equal(y.es$genes, y.expect$genes)
+  expect_equal(y.es$samples, y.expect$samples)
+})
+
+test_that("CPM is like edgeR::cpm", {
+  es <- exampleExpressionSet(do.voom=FALSE)
+  y <- as.DGEList(es, 'exprs')
+
+  c.expect <- cpm(y, log=TRUE, prior.count=5)
+  c.y <- CPM(y, prior.count=5, regularized=FALSE)
+  expect_equivalent(c.y, c.expect) ## CPM adds some attribs
+
+  es$norm.factors <- calcNormFactors(exprs(es))
+  c.es <- CPM(es, prior.count=5, regularized=FALSE)
+  expect_equivalent(c.es, c.expect)
+})
