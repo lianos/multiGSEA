73c151d162cee55d7c376ec2e0636bfebe95693e
lianogls <lianogls@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2015-07-21 19:10:58 +0000

    Adds `inter.gene.cor` parameter to `do.camera`
    See the following post from Gordon on the bioc support forums:
    
      https://support.bioconductor.org/p/70005/#70195
    
    git-svn-id: http://resscm.gene.com/bioinfo/projects/R/trunk/multiGSEA@170250 ea8427e4-251f-0410-985a-fb3ea31d2ee2

--- R/do.camera.R
+++ R/do.camera.R
@@ -6,12 +6,29 @@ validate.x.camera <- validate.X
 
 ##' Runs camera on the given experiment.
 ##'
+##' @section Gene Set Enrichment Methods:
+##'
+##' camera tends to be very conservative, especially when trying to estimate
+##' residual correlation of gene sets from experiments with small N. For this
+##' reason,  Gordon has added the ability provide a prespecified
+##' \code{inter.gene.correlation} value.
+##' \href{https://support.bioconductor.org/p/70005/#70195}{
+##' He suggests to try a small positive number (0.05)}
 do.camera <- function(gsd, x, design, contrast=ncol(design), outdir=NULL,
                       use.cache=TRUE, weights=NULL, use.ranks=FALSE,
-                      allow.neg.cor=TRUE, trend.var=FALSE, sort=FALSE,
+                      allow.neg.cor=TRUE, inter.gene.cor=NULL, trend.var=FALSE,
+                      sort=FALSE,
                       ...) {
   stopifnot(is.conformed(gsd, x))
-  extra.args <- c('use.ranks', 'allow.neg.cor', 'trend.var', 'sort')
+  if (!is.null(inter.gene.cor)) {
+    ## Preset inter.gene.cor values were implemented in limma v3.24.14
+    if (packageVersion('limma') < '3.24.14') {
+      warning("inter.gene.cor values for camera require limma >- 3.24.14",
+              immediate.=TRUE)
+    }
+  }
+  extra.args <- c('use.ranks', 'allow.neg.cor', 'inter.gene.cor', 'trend.var',
+                  'sort')
   cache.fn <- cache.data.fn('camera', design, contrast, extra.args,
                             outdir=outdir, ext='rds')
   gs.idxs <- as.expression.indexes(gsd, value='x.idx')
@@ -23,7 +40,8 @@ do.camera <- function(gsd, x, design, contrast=ncol(design), outdir=NULL,
   } else {
     res <- camera(x, gs.idxs, design, contrast, weights=weights,
                   use.ranks=use.ranks, allow.neg.cor=allow.neg.cor,
-                  trend.var=trend.var, sort=FALSE, ...)
+                  inter.gene.cor=inter.gene.cor, trend.var=trend.var,
+                  sort=FALSE, ...)
     if (is.character(outdir) && isTRUE(file.exists(outdir))) {
       saveRDS(res, cache.fn)
     }
