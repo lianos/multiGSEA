fcd8368b8fa2b3996b706ecf830d4ef52476fae7
Steve Lianoglou <lianoglou.steve@gene.com>
2017-08-14 21:55:14 -0700

    refactored shiny code out to multiGSEA.shiny

--- R/utilities.R
+++ R/utilities.R
@@ -167,20 +167,3 @@ failWith <- function(default=NULL, expr, frame=parent.frame(),
     default
   })
 }
-
-
-##' Rounds every numeric column of a data.table to given precision
-##' @export
-##' @param x a \code{data.table} like thing
-##' @param digits the number of digits to round to
-##' @return the rounded \code{x}
-round.dt <- function(x, digits=3) {
-  stopifnot(is(x, 'data.table'))
-  for (cname in names(x)[sapply(x, is.numeric)]) {
-    vals <- x[[cname]]
-    if (!is.integer(vals)) {
-      x[, (cname) := round(vals, digits=3)]
-    }
-  }
-  x
-}
